3ccfd27845f1070cfefab536b7c8b5ac4249ea25
Rahul Shah <rahulnshah3012@gmail.com>
2022-01-24 19:39:07 -0500

    edited Course.java

--- ExampleOfSerialization/Course.java
+++ ExampleOfSerialization/Course.java
@@ -10,22 +10,6 @@ The following methods should also be provided:
 - A constructor that initializes the name, registration code, and maximum
 number of students
 - Methods to set and retrieve the instructor
-
-
-- A method to search for a student in the course; the search should be
-based on an ID number.
-- A method to add a student to the course. If the course is full, then an
-exception with an appropriate message should be raised (try creating your
-own exception class for this). Also, be sure that the student is not already
-registered in the course. The list of students should be in the order that
-they registered.
-- A method to remove a student from the course. If the student is not
-found, then an exception with an appropriate message should be raised
-(use the same exception class mentioned above).
-- A method that will allow Course objects to be output to a file using
-object serialization
-- A method that will allow Course objects to be read in from a file created
-with Object serializtion
 */
 
 public class Course{
@@ -53,5 +37,41 @@ public class Course{
    {
       return instructor;
    }
+   /*- A method to search for a student in the course; the search should be
+based on an ID number.*/
+   public void search()
+   {
    
+   }
+   /*- A method to add a student to the course. If the course is full, then an
+exception with an appropriate message should be raised (try creating your
+own exception class for this). Also, be sure that the student is not already
+registered in the course. The list of students should be in the order that
+they registered.
+*/
+   public void addStudent()
+   {
+   
+   }
+   /*- A method to remove a student from the course. If the student is not
+found, then an exception with an appropriate message should be raised
+(use the same exception class mentioned above).
+*/
+   public void removeStudent()
+   {
+   
+   }
+   /*- A method that will allow Course objects to be output to a file using
+object serialization
+*/
+   public void outputTo()
+   {
+   
+   }
+   /*- A method that will allow Course objects to be read in from a file created
+with Object serializtion*/
+   public void readFrom()
+   {
+      
+   }  
 }
